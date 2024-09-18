fe9db7f75ed2a2fa89cf8eb38faa44b6244cafbd
keath <keath406@gmail.com>
2024-09-18 13:41:03 +0800

    Add HTML basics example and route setup

---
+++ resources/views/html_example.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Basics</title>
+</head>
+<body>
+    <h1>歡迎來到 HTML 基礎教學！</h1>
+    <p>這是你的第一個 HTML 頁面，它顯示了一些基本的 HTML 元素。</p>
+    <a href="https://www.google.com" target="_blank">點擊這裡訪問 Google</a>
+    <img src="https://via.placeholder.com/150" alt="示例圖片">
+    <form action="/submit-form" method="POST">
+        @csrf
+        <label for="name">姓名：</label>
+        <input type="text" id="name" name="name">
+        <button type="submit">提交</button>
+    </form>
+
+</body>
+</html>
